1cbd318fc7c725d35f8bda6c3e55272984b84455
zjli_QMjYO <zjli@justhealth.cn>
2022-10-20 16:33:50 +0800

    增加注释

--- clip/clip_tools.py
+++ clip/clip_tools.py
@@ -56,8 +56,8 @@ def optimi_txt_source_clip(txt_clip,w,h,duration,text_clip_start,source_clip):
 def add_txt_mask(txt_clip,duration,text_clip_start,w,h):
     txt_w, txt_h = txt_clip.size
     color_size = (txt_w*6//5, txt_h+h*3//20)
-    print("生成遮罩size")
-    colorclip = ColorClip(size=color_size,color=(255, 255, 255))
+    print("生成遮罩size:",color_size)
+    colorclip = ColorClip(size=color_size,color=(0, 0, 0))
     print("生成遮罩clip")
     position = ((w - txt_w)//2 - txt_w//10, ((h - txt_h)//2 - h*3//40) * 9//10)
     print("计算遮罩position")
