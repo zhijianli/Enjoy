c3347e1e514c34764134d179f38f55735837ecb1
zjli_QMjYO <zjli@justhealth.cn>
2023-08-01 16:39:22 +0800

    修改启动文件

--- start-web.sh
+++ start-web.sh
@@ -1,4 +1,3 @@
-pkill -9 python3
 export DEVELOP_ENV=prod
 nohup python3 /github/web/app.py > /data/log.out 2>&1 &
 nohup python3 /github/timing_program/bilibili_contribute.py > /data/log.out 2>&1 &
